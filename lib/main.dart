1ca9bb1feda2060dd6fcf736eac217c1f5f17621
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-05-13 23:25:49 -0300

    Colocando componentes para separar a parte onde sera colocado o grafico

--- lib/main.dart
+++ lib/main.dart
@@ -24,9 +24,22 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Despesas pessoais!"),
       ),
-      body: Center(
-        child: Text("Versão Inicial"),
-      ),
+      body: Column(
+        children: <Widget>[
+
+          Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+          Card(
+            child: Text('Lista de transacoes'),
+          )
+        ],
+        )
     );
   }
 }
